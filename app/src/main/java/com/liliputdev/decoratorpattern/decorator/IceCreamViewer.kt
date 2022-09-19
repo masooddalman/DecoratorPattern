908a6ef8436ddd74f5c8d34919ebf10f2a2a2b22
Masood Dalman <m.dalman68@gmail.com>
2022-09-19 15:19:19 +0300

    buildUi viewer added

--- app/src/main/java/com/liliputdev/decoratorpattern/decorator/IceCreamViewer.kt
+++ app/src/main/java/com/liliputdev/decoratorpattern/decorator/IceCreamViewer.kt
@@ -1,5 +1,9 @@
 package com.liliputdev.decoratorpattern.decorator
 
+import com.liliputdev.decoratorpattern.decorator.iceCreamParts.ConeClassic
+import com.liliputdev.decoratorpattern.decorator.iceCreamParts.ConeSpecial
+import com.liliputdev.decoratorpattern.decorator.iceCreamParts.Sprinkles
+
 /**
  * Created by Masood Dalman on 9/4/2022.
  */
@@ -15,6 +19,23 @@ class IceCreamViewer {
                 (iceCreamDecorator.calculatePrice()
                     ?.times(100))?.div(100f)
             }$"
+        }
+
+        /**
+         * This method returns array of drawable resources based on the input ice cream array.
+         *
+         * the first input item must be ConeClassic() or ConeSpecial() and the last item must be Sprinkles() object
+         *
+         * @return array of drawable resource
+         */
+        fun buildIceCreamResourceList(parts: Array<IceCream>):MutableList<Int>?{
+            if(parts.last() is Sprinkles && (parts.first() is ConeClassic || parts.first() is ConeSpecial)) {
+                val iceCreamDecorator = IceCreamDecorator(parts.reduce { a, b -> b.addPart(a) })
+                return iceCreamDecorator.buildUI()
+            }
+            else {
+                throw NoSuchElementException("the first input item must be ConeClassic() or ConeSpecial() and the last item must be Sprinkles() object")
+            }
         }
     }
 }
